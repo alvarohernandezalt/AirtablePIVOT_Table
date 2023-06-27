e15260308d3c242d1aa15448c11ba0f2d2f93edd
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-06-27 11:24:10 +0200

    version 2

--- PivotTABLE_Inside_AIRTABLE_AUTOMATION.js
+++ PivotTABLE_Inside_AIRTABLE_AUTOMATION.js
@@ -1,11 +1,10 @@
 let Airtable = require('airtable');
-let baseKey = input.config('baseKey');
-let oportunidadesTableName = input.config('oportunidadesTableName');
-let valoresPivotTableName = input.config('valoresPivotTableName');
+let baseKey = 'BASE_ID';
+let oportunidadesTableName = 'HOJA';
+let valoresPivotTableName = 'Valores_PIVOT';
 
-let apiKey = input.config('apiKey');
-let base = new Airtable({ apiKey: apiKey }).base(baseKey);
-let valoresPivotBase = new Airtable({ apiKey: apiKey }).base(baseKey);
+let base = new Airtable().base(baseKey);
+let valoresPivotBase = new Airtable().base(baseKey);
 
 // Get all data from 'Oportunidades' table
 base(oportunidadesTableName).select().all(function(err, records) {
@@ -13,7 +12,7 @@ base(oportunidadesTableName).select().all(function(err, records) {
     console.error(err);
     return;
   }
-  
+
   // Convert records to a DataFrame-like structure
   let data = records.map(record => record.fields);
   let df = data.reduce((acc, row) => {
@@ -47,10 +46,8 @@ base(oportunidadesTableName).select().all(function(err, records) {
   valoresPivotBase(valoresPivotTableName).create(recordsToInsert, function(err, records) {
     if (err) {
       console.error(err);
-      output.set('errorMessage', err.message);
       return;
     }
-    output.set('recordsInserted', records.length);
     console.log('Records inserted:', records.length);
   });
 });
@@ -94,4 +91,9 @@ function aggregate(values, aggfunc) {
     return values.reduce((acc, val) => acc + val, 0);
   } else if (aggfunc === 'mean') {
     let sum = values.reduce((acc, val) => acc + val, 0);
-    return sum / values
+    return sum / values.length;
+  } else {
+    // Add more aggregate functions if needed
+    return null;
+  }
+}
